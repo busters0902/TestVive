4cdd83df51addcc17abe621e312d7eadf9f11197
2F-DT-02\vantan <busters0902@hotmail.co.jp>
2017-10-26 14:45:17 +0900

    更新

---
+++ ViveVR/Assets/Scripts/BallCreator.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCreator : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 pos;
+
+    [SerializeField]
+    Vector3 size;
+
+    [SerializeField]
+    float ballPower;
+
+    [SerializeField]
+    GameObject ballPrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            CreateBall();
+        }
+    }
+
+    GameObject CreateBall()
+    {
+        var obj = Instantiate<GameObject>(ballPrefab);
+        obj.transform.position = CreateBallPostion();
+        var rig = obj.GetComponent<Rigidbody>();
+        rig.velocity = CreateRandomRotation() * Vector3.forward * ballPower;
+        //obj.transform.rotation = CreateRandomRotation();
+        return obj;
+    }
+
+    Vector3 CreateBallPostion()
+    {
+        float x = Random.Range(0, size.x);
+        float y = Random.Range(0, size.y);
+        float z = Random.Range(0, size.z);
+
+        var pos_ = new Vector3(pos.x + x, pos.y + y, pos.z + z);
+        return pos_;
+    }
+
+    Quaternion CreateRandomRotation()
+    {
+        //return Quaternion.FromToRotation(Vector3.zero, new Vector3())
+        return Quaternion.Euler(
+            Random.Range(0f,360f), 
+            Random.Range(0f, 360f), 
+            Random.Range(0f, 360f));
+    }
+
+}
---
+++ ViveVR/Assets/Scripts/BallSpeeder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeeder : MonoBehaviour
+{
+
+    [SerializeField]
+    Rigidbody rig;
+
+    public float speed;
+
+    private void Update()
+    {
+        //float mag = rig.velocity.magnitude;
+        //if (mag < 5.0f)
+        //{
+        //    Destroy(gameObject, 1.0f);
+        //}
+    }
+
+}
